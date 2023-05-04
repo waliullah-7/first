da5dc63c70dc6fba890ce8c7857a4dc4362cf629
waliullah-7 <132615186+waliullah-7@users.noreply.github.com>
2023-05-05 01:12:08 +0500

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+int r;
+
+cout<<"Enter the radius of the circle: ";
+
+  cin>>r;
+int area;
+area=2*3.14*r;
+
+cout<<"The area of the circle is: "<<area;
+
+return 0;
+}
